8667924a898a37ae95dcd0cfb034a94e22231cc2
Ivan Manko <bahzhij@gmail.com>
2018-08-27 22:33:14 +0300

    fix code formatting

--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -1,72 +1,86 @@
 //task 1
 function findType(data) {
-    return typeof data;
-  }
-  
-  //task 2
-  function forEach(basicArray, func) {
-    for (let i = 0; i < basicArray.length; i++) {
-      func(basicArray[i]);
-    }
-  }
-  
-  //task 3
-  function map(basicArray, func) {
-    let modifiedArray = [];
-    forEach(basicArray, function(el) {
-      modifiedArray.push(func(el));
-    });
-    return modifiedArray;
-  }
-  
-  //task 4
-  function filter(basicArray, func) {
-    let modifiedArray = [];
-    forEach(basicArray, function(el) {
-      if (func(el)) {
-        modifiedArray.push(el);
-      }
-    });
-    return modifiedArray;
-  }
-  
-  //task 5
-  function getAdultAppleLovers(data) {
-    return map(
-      filter(data, function(el) {
-        return el.favoriteFruit === 'apple' && el.age >= 18;
-      }),
-      function(el) {
-        return el.name;
-      }
-    );
-  }
-  
-  //task 6
-  function keys(basicArray) {
-    let keysArray = [];
-    for (let key in basicArray) {
-      if (basicArray.hasOwnProperty(key)) {
-        keysArray.push(key);
-      }
-    }
-    return keysArray;
-  }
-  
-  //task 7
-  function values(basicArray) {
-    let valuesArray = [];
-    for (let key in basicArray) {
-      if (basicArray.hasOwnProperty(key)) {
-        valuesArray.push(basicArray[key]);
-      }
-    }
-    return valuesArray;
-  }
-  
-  //task 8
-  function showFormattedDate(date) {
-    let MONTH_NAMES = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    return `It is ${date.getDate()} of ${MONTH_NAMES[date.getMonth()]}, ${date.getFullYear()};`;
-  }
-  
+	return typeof data;
+}
+
+//task 2
+function forEach(basicArray, func) {
+	for (let i = 0; i < basicArray.length; i++) {
+		func(basicArray[i]);
+	}
+}
+
+//task 3
+function map(basicArray, func) {
+	let modifiedArray = [];
+	forEach(basicArray, function(el) {
+		modifiedArray.push(func(el));
+	});
+	return modifiedArray;
+}
+
+//task 4
+function filter(basicArray, func) {
+	let modifiedArray = [];
+	forEach(basicArray, function(el) {
+		if (func(el)) {
+			modifiedArray.push(el);
+		}
+	});
+	return modifiedArray;
+}
+
+//task 5
+function getAdultAppleLovers(data) {
+	return map(
+		filter(data, function(el) {
+			return el.favoriteFruit === 'apple' && el.age >= 18;
+		}),
+		function(el) {
+			return el.name;
+		}
+	);
+}
+
+//task 6
+function keys(basicArray) {
+	let keysArray = [];
+	for (let key in basicArray) {
+		if (basicArray.hasOwnProperty(key)) {
+			keysArray.push(key);
+		}
+	}
+	return keysArray;
+}
+
+//task 7
+function values(basicArray) {
+	let valuesArray = [];
+	for (let key in basicArray) {
+		if (basicArray.hasOwnProperty(key)) {
+			valuesArray.push(basicArray[key]);
+		}
+	}
+	return valuesArray;
+}
+
+//task 8
+function showFormattedDate(date) {
+	let MONTH_NAMES = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec'
+	];
+	return `It is ${date.getDate()} of ${
+		MONTH_NAMES[date.getMonth()]
+	}, ${date.getFullYear()};`;
+}
