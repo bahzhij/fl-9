4bf119e3583788be7c36a1bee3253950826abcff
Ivan Manko <bahzhij@gmail.com>
2018-10-09 23:26:51 +0300

    fix formatting

--- FL_9_5_homework_es-next/homework/tower-of-babel/destructure.js
+++ FL_9_5_homework_es-next/homework/tower-of-babel/destructure.js
@@ -1,12 +1,12 @@
 const json = {
-  name: {
-    first: 'Yosuke',
-    family: process.argv[2]
+  "name": {
+    "first": "Yosuke",
+    "family": process.argv[2]
   },
-  birth: {
-    year: 1982,
-    month: 12,
-    day: process.argv[3]
+  "birth": {
+    "year": 1982,
+    "month": 12,
+    "day": process.argv[3]
   }
 };
 
